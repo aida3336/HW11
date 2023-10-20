680525856fabb01681becabb579fe5d00674e85e
Aida <primkulovaaida@gmail.com>
2023-10-21 02:07:10 +0600

    Add buttons - Login and Forgot your password

--- HW11/ViewController.swift
+++ HW11/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController {
         textField.textAlignment = .center
         textField.textColor = .black
         textField.backgroundColor = .white
+        textField.font = .boldSystemFont(ofSize: 14)
         textField.setLeftIcon(icon)
         textField.setRightIcon(checked)
         return textField
@@ -39,9 +40,28 @@ class ViewController: UIViewController {
         textFieldPassword.textColor = .black
         textFieldPassword.backgroundColor = .white
         textFieldPassword.setLeftIcon(icon)
+        textFieldPassword.font = .boldSystemFont(ofSize: 14)
         return textFieldPassword
     }()
     
+    private lazy var button: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Login", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor(named: "buttonBackgroundColor")
+        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+
+        return button
+    }()
+    
+    private lazy var buttonLink: UIButton = {
+        let buttonLink = UIButton(type: .system)
+        buttonLink.setTitle("Forgot your password?", for: .normal)
+        buttonLink.setTitleColor(.white, for: .normal)
+        buttonLink.backgroundColor = .none
+        buttonLink.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        return buttonLink
+    }()
 // Lifecycle
     
     override func viewDidLoad() {
@@ -54,6 +74,9 @@ class ViewController: UIViewController {
             super.viewDidLayoutSubviews()
             textField.layer.cornerRadius = 25
             textFieldPassword.layer.cornerRadius = 25
+            button.layer.cornerRadius = 25
+            buttonLink.layer.borderWidth = 0
+
         }
         viewDidLayoutSubviews()
         
@@ -62,17 +85,19 @@ class ViewController: UIViewController {
     // Setup Views
     
     private func setupViews() {
-        view.backgroundColor = .systemPurple
+        view.backgroundColor = UIColor(named: "backgroundColor")
         view.addSubview(titleLabel)
         view.addSubview(textField)
         view.addSubview(textFieldPassword)
+        view.addSubview(button)
+        view.addSubview(buttonLink)
     }
     // Setup Constraints
     private func setupConstraints() {
         titleLabel.snp.makeConstraints { make in
             make.centerX.equalTo(view)
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(50)
-        }∏
+        }
         textField.snp.makeConstraints { make in
             make.centerX.equalTo(view)
             make.top.equalTo(titleLabel.snp.bottom).offset(50)
@@ -87,14 +112,26 @@ class ViewController: UIViewController {
             make.trailing.equalToSuperview().offset(-50)
             make.height.equalTo(50)
         }
+        button.snp.makeConstraints {make in
+            make.top.equalTo(textFieldPassword.snp.bottom).offset(60)
+            make.leading.equalToSuperview().offset(50)
+            make.trailing.equalToSuperview().offset(-50)
+            make.height.equalTo(50)
+        }
+        buttonLink.snp.makeConstraints {make in
+            make.top.equalTo(button.snp.bottom).offset(5)
+            make.leading.equalToSuperview().offset(50)
+            make.trailing.equalToSuperview().offset(-50)
+            make.height.equalTo(40)
+        }
     }
 }
 
 extension UITextField {
     func setLeftIcon(_ image: UIImage?) {
-        let iconView = UIImageView(frame: CGRect(x: 30, y: 5, width: 20, height: 20))
+        let iconView = UIImageView(frame: CGRect(x: 20, y: 5, width: 20, height: 20))
         iconView.image = image
-        let iconContainerView: UIView = UIView(frame: CGRect(x: 30, y: 0, width: 30, height: 30))
+        let iconContainerView: UIView = UIView(frame: CGRect(x: 20, y: 0, width: 30, height: 30))
         iconContainerView.addSubview(iconView)
         leftView = iconContainerView
         leftViewMode = .always

2d5e5bcd1b429970d03178fb34e0b0a17c1f1379
Aida <primkulovaaida@gmail.com>
2023-10-20 22:02:49 +0600

    Add title label.

--- HW11/ViewController.swift
+++ HW11/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
-
+import SnapKit
 class ViewController: UIViewController {
-
+// UI
+    private lazy var titleLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Login"
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = .boldSystemFont(ofSize: 26)
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+   setupViews()
+  setupConstraints()
     }
 
-
+// SETUP VIEWS
+    
+    private func setupViews() {
+        view.backgroundColor = .systemPurple
+        view.addSubview(titleLabel)
+    }
+// Setup Constraints
+    private func setupConstraints() {
+        titleLabel.snp.makeConstraints { make in
+            make.centerX.equalTo(view)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(50)
+        }
+    }
 }
 

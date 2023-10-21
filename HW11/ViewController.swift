1a61fc1933280e930fe1c4ebefd3b78eb3d6290f
Aida <primkulovaaida@gmail.com>
2023-10-21 21:50:42 +0600

    Add buttons Facebook. twitter, sign up

--- HW11/ViewController.swift
+++ HW11/ViewController.swift
@@ -22,8 +22,8 @@ class ViewController: UIViewController {
         let textField = UITextField()
         let icon = UIImage(named: "login")
         let checked = UIImage(named: "checked")
-        textField.placeholder = "Write your login"
-        textField.textAlignment = .center
+        textField.placeholder = "      Write your login"
+        textField.textAlignment = .left
         textField.textColor = .black
         textField.backgroundColor = .white
         textField.font = .boldSystemFont(ofSize: 14)
@@ -35,8 +35,8 @@ class ViewController: UIViewController {
     private lazy var textFieldPassword: UITextField = {
         let textFieldPassword = UITextField()
         let icon = UIImage(named: "password")
-        textFieldPassword.placeholder = "Password"
-        textFieldPassword.textAlignment = .center
+        textFieldPassword.placeholder = "      Password"
+        textFieldPassword.textAlignment = .left
         textFieldPassword.textColor = .black
         textFieldPassword.backgroundColor = .white
         textFieldPassword.setLeftIcon(icon)
@@ -50,7 +50,6 @@ class ViewController: UIViewController {
         button.setTitleColor(.white, for: .normal)
         button.backgroundColor = UIColor(named: "buttonBackgroundColor")
         button.titleLabel?.font = .boldSystemFont(ofSize: 14)
-
         return button
     }()
     
@@ -62,6 +61,66 @@ class ViewController: UIViewController {
         buttonLink.titleLabel?.font = .boldSystemFont(ofSize: 14)
         return buttonLink
     }()
+    
+    private lazy var connectWithLabel: UILabel = {
+        let connectWithLabel = UILabel()
+        connectWithLabel.text = "-------- or connect with --------"
+        connectWithLabel.textColor = .black
+        connectWithLabel.textAlignment = .center
+        connectWithLabel.font = .boldSystemFont(ofSize: 14)
+        return connectWithLabel
+    }()
+    
+    private lazy var facebookButton: UIButton = {
+        let facebookButton = UIButton(type: .system)
+        facebookButton.setTitle("Facebook", for: .normal)
+        facebookButton.setTitleColor(.white, for: .normal)
+        facebookButton.backgroundColor = .systemBlue
+        facebookButton.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        facebookButton.setImage(UIImage(systemName: "facebookIcon"), for: .normal)
+        return facebookButton
+    }()
+    private lazy var textFieldFB: UITextField = {
+       let textFieldFB = UITextField()
+        let facebookIcon = UIImage(named: "facebookIcon")
+        textFieldFB.setLeftIcon(facebookIcon)
+        return textFieldFB
+    }()
+    private lazy var twitterButton: UIButton = {
+        let twitterButton = UIButton(type: .system)
+        twitterButton.setTitle("Twitter", for: .normal)
+        twitterButton.setTitleColor(.white, for: .normal)
+        twitterButton.backgroundColor = UIColor(named: "buttonBackgroundColor")
+        twitterButton.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        twitterButton.setImage(UIImage(systemName: "twitterIcon"), for: .normal)
+        return twitterButton
+    }()
+    
+    private lazy var textFieldTw: UITextField = {
+       let textFieldTw = UITextField()
+        let twitterIcon = UIImage(named: "twitterIcon")
+        textFieldTw.setLeftIcon(twitterIcon)
+        return textFieldTw
+    }()
+    
+    private lazy var accountLabel: UILabel = {
+        let accountLabel = UILabel()
+        accountLabel.text = "Don't have account?"
+        accountLabel.textColor = .black
+        accountLabel.textAlignment = .center
+        accountLabel.font = .boldSystemFont(ofSize: 14)
+        return accountLabel
+    }()
+    
+    private lazy var signUpLink: UIButton = {
+        let signUpLink = UIButton(type: .system)
+        signUpLink.setTitle("Sign up", for: .normal)
+        signUpLink.setTitleColor(.blue, for: .normal)
+        signUpLink.backgroundColor = .none
+        signUpLink.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        return signUpLink
+    }()
+    
 // Lifecycle
     
     override func viewDidLoad() {
@@ -76,6 +135,8 @@ class ViewController: UIViewController {
             textFieldPassword.layer.cornerRadius = 25
             button.layer.cornerRadius = 25
             buttonLink.layer.borderWidth = 0
+            facebookButton.layer.cornerRadius = 15
+            twitterButton.layer.cornerRadius = 15
 
         }
         viewDidLayoutSubviews()
@@ -91,6 +152,13 @@ class ViewController: UIViewController {
         view.addSubview(textFieldPassword)
         view.addSubview(button)
         view.addSubview(buttonLink)
+        view.addSubview(connectWithLabel)
+        view.addSubview(facebookButton)
+        view.addSubview(textFieldFB)
+        view.addSubview(twitterButton)
+        view.addSubview(textFieldTw)
+        view.addSubview(accountLabel)
+        view.addSubview(signUpLink)
     }
     // Setup Constraints
     private func setupConstraints() {
@@ -124,6 +192,48 @@ class ViewController: UIViewController {
             make.trailing.equalToSuperview().offset(-50)
             make.height.equalTo(40)
         }
+        connectWithLabel.snp.makeConstraints {make in
+            make.top.equalTo(buttonLink.snp.bottom).offset(190)
+            make.leading.equalToSuperview().offset(50)
+            make.trailing.equalToSuperview().offset(-50)
+            make.height.equalTo(40)
+        }
+        facebookButton.snp.makeConstraints {make in
+            make.top.equalTo(connectWithLabel.snp.bottom).offset(15)
+            make.leading.equalToSuperview().offset(30)
+            make.trailing.equalToSuperview().offset(-210)
+            make.height.equalTo(30)
+        }
+        textFieldFB.snp.makeConstraints {make in
+                make.top.equalTo(connectWithLabel.snp.bottom).offset(15)
+                make.leading.equalToSuperview().offset(25)
+                make.trailing.equalToSuperview().offset(-320)
+                make.height.equalTo(30)
+            }
+        twitterButton.snp.makeConstraints {make in
+            make.top.equalTo(connectWithLabel.snp.bottom).offset(15)
+            make.leading.equalToSuperview().offset(210)
+            make.trailing.equalToSuperview().offset(-30)
+            make.height.equalTo(30)
+        }
+        textFieldTw.snp.makeConstraints {make in
+                make.top.equalTo(connectWithLabel.snp.bottom).offset(15)
+                make.leading.equalToSuperview().offset(205)
+                make.trailing.equalToSuperview().offset(-180)
+                make.height.equalTo(30)
+            }
+        accountLabel.snp.makeConstraints {make in
+                make.top.equalTo(twitterButton.snp.bottom).offset(30)
+                make.leading.equalToSuperview().offset(30)
+                make.trailing.equalToSuperview().offset(-100)
+                make.height.equalTo(30)
+            }
+        signUpLink.snp.makeConstraints {make in
+                make.top.equalTo(twitterButton.snp.bottom).offset(30)
+                make.leading.equalToSuperview().offset(180)
+                make.trailing.equalToSuperview().offset(-30)
+                make.height.equalTo(30)
+            }
     }
 }
 
@@ -144,4 +254,14 @@ extension UITextField {
         rightView = iconContainerView
         rightViewMode = .always
     }
+    func setImageToButton(icon: String) {
+            let logo = UIImageView(image: UIImage(named: icon))
+            logo.contentMode = .scaleAspectFit
+            logo.translatesAutoresizingMaskIntoConstraints = false
+            self.addSubview(logo)
+            logo.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10).isActive = true
+            logo.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+            logo.heightAnchor.constraint(equalToConstant: 30).isActive = true
+            logo.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        }
 }
